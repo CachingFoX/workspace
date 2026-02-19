58cea10c3ccba9c47ca8a4686b55d3ce28f3d319
CachingFoX <cachingfox@cachingfox.org>
2026-02-19 23:19:33 +0000

    improve trackable details

--- src/components/TrackableDetails.vue
+++ src/components/TrackableDetails.vue
@@ -2,6 +2,7 @@
 import { computed,ref, watch, onBeforeMount, onMounted } from 'vue'
 // prime vue
 import Button from 'primevue/button';
+import ButtonGroup from 'primevue/buttongroup';
 import ConfirmDialog from 'primevue/confirmdialog';
 import { useRouter, useRoute } from 'vue-router';
 import { useToast } from "primevue/usetoast";
@@ -67,6 +68,21 @@ function onAddTrackable() {
       <TrackableTitle/>
     </template>
     <template v-slot:mainstage>
+      <div class="px-2 py-2">
+        <div class="flex align-items-center">
+          <div class="flex-grow-1">
+            <ButtonGroup>
+              <Button @click="goToListItem(storeTrackable.id)" label="Zurück" icon="pi pi-angle-double-left"/>
+              <Button @click="" label="Refresh Stammdaten" icon="pi pi-refresh"/>
+            </ButtonGroup>
+          </div>
+          <div>
+            <ButtonGroup>
+              <Button @click="onDelete" severity="danger" icon="pi pi-trash" label="Löschen"/>
+            </ButtonGroup>
+          </div>
+        </div>
+       </div>
       <TrackableBase/>
       <div v-if="!storeTrackable.complete" class="px-2">
         <Button @click="onAddTrackable">Neuen Trackable hinzufügen</Button>
@@ -78,25 +94,6 @@ function onAddTrackable() {
         <TrackableLinkedTrackables property="linkedTrackables"/>
         <TrackableSameSeries :series="storeTrackable.series" :self="storeTrackable.private_code"/>
         <TrackableSameTag :series="storeTrackable.series" :self="storeTrackable.private_code" :tags="storeTrackable.tags"/>
-      </div>
-    </template>
-    <template v-slot:footer>
-      <div class="flex align-items-center pt-2 pb-2">
-        <div class="flex-grow-0 flex-shrink-0">
-          <Button @click="goToListItem(storeTrackable.id)" label="Zurück" icon="pi pi-angle-double-left"/>
-        </div>
-        <div class="flex-grow-1 flex-shrink-1" style="text-align: center;">
-          <Button @click="onDelete" severity="danger" icon="pi pi-trash" label="Löschen"/>
-        </div>
-        <div class="flex-grow-0 flex-shrink-0" style="text-align: right;">
-          <span v-show="storeTrackable.progress" class="pi pi-spin pi-spinner" style="font-size: 1.5rem"/>
-        </div>
-        <!--
-        <div class="flex-grow-0 flex-shrink-0">
-          <Button @click="" icon="pi pi-refresh" label="Aktualisieren" class="mr-2"/>
-          <Button @click="" icon="pi pi-save" label="Speichern" class="mr-2"/>
-        </div>
-        -->
       </div>
     </template>
   </BaseLayout>
