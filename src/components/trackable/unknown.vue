e3eec8371f0a15deac24c7fb2c00b91fc4bc3144
CachingFoX <cachingfox@cachingfox.org>
2026-02-22 06:26:08 +0000

    improve unknown trackable component layout

--- src/components/trackable/unknown.vue
+++ src/components/trackable/unknown.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Button from 'primevue/button';
 import { useTrackableStore } from "@/di/trackables.js"
+import { computed } from 'vue';
 
 const storeTrackable = useTrackableStore();
 
@@ -11,12 +12,34 @@ const props = defineProps({
 function onClick() {
   storeTrackable.readTrackableHQ(props.trackingNumber);
 }
+
+const isPublicCode = computed(()=>{
+  return props.trackingNumber.startsWith("TB")
+})
 </script>
 
 <template>
-  Unknown {{ props.trackingNumber }}
-  <Button @click="onClick">Abfragen</Button>
+  <div class="flex flex-column align-items-center justify-content-center row-gap-0" style="height: 100%;">
+    <div class="font-bold font-huge">?</div>
+    <div class="font-big">Trackable <b>{{props.trackingNumber}}</b> ist nicht in der Datenbank enthalten</div>
+    <div class="font-normal mt-2" v-show="!isPublicCode">
+      Den Trackable bei www.geocaching.com abfragen?
+    </div>
+    <Button class="mt-2" v-show="!isPublicCode" label="Abfragen" @click="onClick"/>
+  </div>
 </template>
 
 <style scoped>
+div > div {
+  text-align: center;
+}
+.font-huge {
+  font-size: 30vh;
+}
+.font-big {
+  font-size: 24pt;
+}
+.font-normal {
+  font-size: 16pt;
+}
 </style>
