3428edbe66aed50b50702ebaeac54a2efeede025
CachingFoX <cachingfox@cachingfox.org>
2026-03-07 04:46:40 +0000

    refactoring

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,14 +1,15 @@
 
+export const API_ENVIRONMENT = {
+  PRODUCTIVE: "productive",
+  DEVELOPMENT: "development"
+}
 
-const API_URL_PRODUCTIVE = "productive"
-const API_URL_DEVELOPMENT = "development"
-
-const API_URLS = {
-  [API_URL_PRODUCTIVE]: {
+const API_ENVIRONMENT_CONFIGURATIONS = {
+  [API_ENVIRONMENT.PRODUCTIVE]: {
     name: 'Productive',
     baseUrl: 'http://localhost:8000' // 7000
   },
-  [API_URL_DEVELOPMENT]: {
+  [API_ENVIRONMENT.DEVELOPMENT]: {
     name: 'Development',
     baseUrl: 'http://localhost:8000' // 9000
   },
@@ -17,7 +18,7 @@ const API_URLS = {
 const STORAGE_KEY_API_ENVIRONMENT = "trackables.api.environment"
 
 export function getApiEnvironment() {
-  return localStorage.getItem(STORAGE_KEY_API_ENVIRONMENT) ?? API_URL_PRODUCTIVE
+  return localStorage.getItem(STORAGE_KEY_API_ENVIRONMENT) ?? API_ENVIRONMENT.PRODUCTIVE
 }
 
 export function setApiEnvironment(env) {
@@ -25,6 +26,6 @@ export function setApiEnvironment(env) {
 }
 
 export function getApiBaseUrl() {
-  console.log("API config:", getApiEnvironment(), "with BaseURL", API_URLS[getApiEnvironment()].baseUrl)
-  return API_URLS[getApiEnvironment()].baseUrl
+  console.log("API config:", getApiEnvironment(), "with BaseURL", API_ENVIRONMENT_CONFIGURATIONS[getApiEnvironment()].baseUrl)
+  return API_ENVIRONMENT_CONFIGURATIONS[getApiEnvironment()].baseUrl
 }
